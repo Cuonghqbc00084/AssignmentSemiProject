5a84eabc96f42c5c2f2cdc4d975816bd6f21c032
Your Name <you@example.com>
2023-07-21 10:55:15 +0700

    Upload Invoice + detail file

---
+++ src/Controller/InvoiceDetailController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\OrderItem;
+use App\Form\InvoiceDetailFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class InvoiceDetailController extends AbstractController
+{
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+        {
+        }
+    #[Route('/invoice_detail/detail', name: 'app_invoice_detail')]
+    public function index(): Response
+    {
+        $invd = new OrderItem();
+        $form = $this->createForm(InvoiceDetailFormType::class, $invd);
+        $form->handleRequest($req);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $em->persist($data);
+            $em->flush();
+        
+        return new RedirectResponse($this->urlGenerator->generate('app_ds_Invoicedetail'));
+
+        }
+
+        return $this->render('invoice_detail/index.html.twig', [
+            'invoicedetail_form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/invoicedetail/ds', name: 'app_ds_Invoicedetail')]
+    public function list_invd(EntityManagerInterface $em): Response
+    {
+        $query = $em->createQuery('SELECT invd FROM App\Entity\OrderItem invd');
+        $lSp = $query->getResult();
+        return $this->render('invoice_detail/list.html.twig', [
+            "data"=>$lSp
+        ]);
+    }
+}
---
+++ src/Form/InvoiceDetailFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\OrderItem;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class InvoiceDetailFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantity',NumberType::class)
+            ->add('price',NumberType::class)
+            ->add('o',NumberType::class)
+            ->add('item',NumberType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => OrderItem::class,
+        ]);
+    }
+}
