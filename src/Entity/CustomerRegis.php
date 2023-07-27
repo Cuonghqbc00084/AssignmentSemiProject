432eb53638ec080a496a20ff484764a3c0c83988
Your Name <you@example.com>
2023-07-27 12:20:26 +0700

    Login error

--- src/Entity/CustomerRegis.php
+++ src/Entity/CustomerRegis.php
@@ -49,7 +49,7 @@ class CustomerRegis implements  UserInterface, PasswordAuthenticatedUserInterfac
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        // $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
