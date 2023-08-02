65a1c173d378040d2bbeb39d169e926f05fbe6a1
Cuong <cuonghqbc00084@fpt.edu.vn>
2023-08-02 12:40:48 +0700

    Fix code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -29,5 +30,4 @@ class SecurityController extends AbstractController
     {
         $request->getSession()->invalidate();
     }
-    
 }
