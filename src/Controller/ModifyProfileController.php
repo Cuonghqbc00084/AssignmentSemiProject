1263c02da323b0f5afaa3c93661a283bb40fa43a
daint123 <133869027+daint123@users.noreply.github.com>
2023-08-11 09:37:40 +0700

    ModifyProfile

--- src/Controller/ModifyProfileController.php
+++ src/Controller/ModifyProfileController.php
@@ -1,29 +1,69 @@
 <?php
 
+// src/Controller/ProfileController.php
+
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\ProfileFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-// use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+
 
 class ModifyProfileController extends AbstractController
 {
-    #[Route('/modify/profile', name: 'app_modify_profile')]
-    public function index(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/profile/edit', name: 'app_edit_profile')]
+    public function editProfile(Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     if ($form->get('plainPassword')->getData()) {
+        //         $hashedPassword = $userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData());
+        //         $user->setPassword($hashedPassword);
+        //     }
+
+        //     $this->entityManager->flush();
+
+        //     return $this->redirectToRoute('app_profile');
+        // }
+
         return $this->render('modify_profile/index.html.twig', [
-            'controller_name' => 'ModifyProfileController',
+            'form' => $form->createView(),
         ]);
     }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function showProfile(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
 }
-    
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,20 +4,79 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ProfileFormType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+
+use App\Entity\User;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
-    {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
+
+        // #[Route('profile', name: 'app_profile')]
+        // public function index(): Response
+        // {
+        //     return $this->render('profile/index.html.twig', [
+        //         'controller_name' => 'ModifyProfileController',
+        //     ]);
+        // }
+        #[Route('/profile', name: 'app_profile')]
+        public function profile(): Response
+        {
+            $user = $this->getUser();
+            return $this->render('profile/list.html.twig', [
+                'user' => $user,
+            ]);
+        
+        }
+
+
+    // #[Route('/profile/{id}', name: 'app_edit_profile')]
+    // public function edit(EntityManagerInterface $em, int $id, Request $req): Response
+    // {
+    //     $pf = $em->find(User::class, $id); 
+    //     // tìm kiếm khóa chính id
+    //     $form = $this->createForm(SanPhamType::class, $pf);
+    //     $form->handleRequest($req);
+
+    //     if($form->isSubmitted() && $form->isValid()) {
+    //         $data = $form->getData();
+
+            
+    //         $pf->setFirstName($data->getFirstName())->setLastName($data->getLastName());
+    //         $em->flush();
+            
+    //         return new RedirectResponse($this->urlGenerator->generate('app_profile'));
+        
+    // }
+    // return $this->render('profile/list.html.twig', [
+    //     'profile_form' => $form->createView(),
+    //     ]);
+    // }
+    
     }
-}
+
+
+
+    // #[Route('/profile/edit', name: 'app_edit_profile')]
+    // public function editProfile(Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    // {
+    //     $user = $this->getUser();
+    //     $form = $this->createForm(User::class, $user);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $data = $form->getData();
+
+            
+    //         $user->setFirstName($data->getFirstName())->setPrice($data->getPrice());
+    //         $em->flush();
     
 
 
